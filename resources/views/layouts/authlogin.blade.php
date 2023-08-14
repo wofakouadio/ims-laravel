1cad4e64f23b55927c3b134337e8bdc959b36ae3
Francis Bennett <kouamekouadiofrancisbennett@gmail.com>
2023-08-14 21:12:28 +0000

    second commit

---
+++ resources/views/layouts/authlogin.blade.php
@@ -0,0 +1,124 @@
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <title>Login | IMS - Laravel</title>
+
+    <meta name="author" content="https://wofakouadio.github.io">
+    <meta name="robots" content="noindex, nofollow">
+
+    <!-- Icons -->
+    <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+{{--    <link rel="shortcut icon" href="assets/media/favicons/favicon.png">--}}
+{{--    <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">--}}
+{{--    <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">--}}
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('favicon/apple-icon-57x57.png')}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{asset('favicon/apple-icon-60x60.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('favicon/apple-icon-72x72.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('favicon/apple-icon-76x76.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('favicon/apple-icon-114x114.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('favicon/apple-icon-120x120.png')}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('favicon/apple-icon-144x144.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('favicon/apple-icon-152x152.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon/apple-icon-180x180.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('favicon/android-icon-192x192.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('favicon/favicon-96x96.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('favicon/manifest.json')}}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="{{asset('favicon/ms-icon-144x144.png')}}">
+    <meta name="theme-color" content="#ffffff">
+    <!-- END Icons -->
+
+    <!-- Stylesheets -->
+    <!-- Codebase framework -->
+    <link rel="stylesheet" id="css-main" href="{{asset('assets/css/codebase.min.css')}}">
+
+    <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+    <!-- <link rel="stylesheet" id="css-theme" href="assets/css/themes/flat.min.css"> -->
+    <!-- END Stylesheets -->
+</head>
+
+<body>
+<!-- Page Container -->
+<!--
+  Available classes for #page-container:
+
+  GENERIC
+
+    'remember-theme'                            Remembers active color theme and dark mode between pages using localStorage when set through
+                                                - Theme helper buttons [data-toggle="theme"],
+                                                - Layout helper buttons [data-toggle="layout" data-action="dark_mode_[on/off/toggle]"]
+                                                - ..and/or Codebase.layout('dark_mode_[on/off/toggle]')
+
+  SIDEBAR & SIDE OVERLAY
+
+    'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+    'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+    'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+    'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+    'sidebar-dark'                              Dark themed sidebar
+
+    'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+    'side-overlay-o'                            Visible Side Overlay by default
+
+    'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+    'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+  HEADER
+
+    ''                                          Static Header if no class is added
+    'page-header-fixed'                         Fixed Header
+
+  HEADER STYLE
+
+    ''                                          Classic Header style if no class is added
+    'page-header-modern'                        Modern Header style
+    'page-header-dark'                          Dark themed Header (works only with classic Header style)
+    'page-header-glass'                         Light themed Header with transparency by default
+                                                (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+    'page-header-glass page-header-dark'        Dark themed Header with transparency by default
+                                                (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+  MAIN CONTENT LAYOUT
+
+    ''                                          Full width Main Content if no class is added
+    'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+    'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+
+  DARK MODE
+
+    'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
+-->
+<div id="page-container" class="main-content-boxed">
+
+    <!-- Main Container -->
+    @yield('content')
+    <!-- END Main Container -->
+</div>
+<!-- END Page Container -->
+
+<!--
+    Codebase JS
+
+    Core libraries and functionality
+    webpack is putting everything together at assets/_js/main/app.js
+-->
+<script src="{{asset('assets/js/codebase.app.min.js')}}"></script>
+
+<!-- jQuery (required for Select2 + jQuery Validation plugins) -->
+<script src="{{asset('assets/js/lib/jquery.min.js')}}"></script>
+
+<!-- Page JS Plugins -->
+<script src="{{asset('assets/js/plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+
+<!-- Page JS Code -->
+<script src="{{asset('assets/js/pages/op_auth_signin.min.js')}}"></script>
+</body>
+</html>
